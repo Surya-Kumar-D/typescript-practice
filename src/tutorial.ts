e691c816f9dc11c9917f02fc2c4f66d5090a3a29
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-10-14 18:34:24 +0530

    first commit

---
+++ src/tutorial.ts
@@ -0,0 +1,81 @@
+// console.log("typeScript tutorial");
+
+// let awesomeName: string = "shakeAndBake";
+
+// awesomeName = "Something";
+// console.log(awesomeName);
+
+// let amount: number = 10;
+
+// amount = 2 - 1;
+
+// let isAwesome: boolean = true;
+// isAwesome = false;
+
+// isAwesome = "shakeAndBake";
+
+// let quote: string = "Welcome to the course";
+
+// quote = quote.toUpperCase();
+
+// console.log(quote);
+
+// let num: number = 20;
+
+// console.log(num / 2);
+
+// quote = true;
+
+// num = "Hello World";
+
+// console.log(quote);
+
+// console.log(num);
+
+// let tax: number | string = 10;
+// tax = 100;
+// tax = "$10";
+
+// let requestStatus: "pending" | "Success" | "error" = "pending";
+
+// requestStatus = "pending";
+
+// let notSure: any = 4;
+
+// notSure = "maybe a string instead";
+
+// notSure = false;
+
+// let random;
+
+// random = "ironman"
+
+// const books = ["1984", "Brave New World", "Fahrenheit 451"];
+
+// let foundBook: string | undefined;
+
+// for (let book of books) {
+//   if (book === "1984") {
+//     foundBook = book;
+//     foundBook = foundBook.toUpperCase();
+//     break;
+//   }
+// }
+
+// console.log(foundBook?.length);
+
+// console.log(foundBook);
+
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+
+orderStatus = "shipped";
+
+orderStatus = "delivered";
+
+console.log(orderStatus);
+
+let discount: number | string = 20;
+
+discount = "20%";
+
+console.log(discount);
